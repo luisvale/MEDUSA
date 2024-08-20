1f321daa2f21e3976b972985c37acfe7af86640b
Valerio2385 <luis.valerio@bpc-lat.com>
2024-08-20 16:18:54 -0600

    s

---
+++ medusa/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Ajuste_PACASA',
+    'version' : '2.3',
+    'author':'s',
+    'category': 'Sales',
+    'maintainer': '',
+
+        You can directly create invoice and set done to delivery order by single click
+
+    """,
+    'license': 'LGPL-3',
+    'depends' : ['sale_management','stock'],
+    'data': [
+        'views/stock_warehouse.xml',
+    ],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/main_screen.png'],
+
+}
