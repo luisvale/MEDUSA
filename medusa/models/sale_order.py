ccc57c433854078021d1917981f12da3b1f2c281
Valerio2385 <luis.valerio@bpc-lat.com>
2024-10-24 09:48:08 -0600

    Update sale_order.py

--- medusa/models/sale_order.py
+++ medusa/models/sale_order.py
@@ -1,8 +1,5 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-import logging
-
-_logger = logging.getLogger(__name__)
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
